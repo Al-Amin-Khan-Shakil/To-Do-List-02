494e427126d80fdab77ac63d2aed591a55af4326
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-08-24 22:46:41 +0600

    Create add and delete task function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style.css';
+import ToDo from './modules/todoList.js';
+import { taskList } from './modules/variables.js';
 
+const addButton = document.getElementById('add-btn');
+const todo = new ToDo();
+
+addButton.addEventListener('click', todo.addTask);
+
+taskList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('del-btn')) {
+    const index = parseInt(event.target.dataset.index, 10) - 1;
+    todo.deleteTask(index);
+  }
+});
+
+window.addEventListener('load', todo.createTaskList);
---
+++ src/modules/todoList.js
@@ -0,0 +1,79 @@
+import Task from './taskObject.js';
+import { inputTask, taskList } from './variables.js';
+
+export default class ToDo {
+  constructor() {
+    this.taskData = [];
+  }
+
+  addData = () => {
+    const taskValue = inputTask.value.trim();
+
+    if (taskValue) {
+      const newTask = new Task((this.taskData.length + 1), taskValue);
+      this.taskData.push(newTask);
+    }
+
+    document.getElementById('input-task').value = '';
+  }
+
+  setToLocal = () => {
+    localStorage.setItem('taskCollection', JSON.stringify(this.taskData));
+  };
+
+  getFromLocal = () => {
+    const getData = localStorage.getItem('taskCollection');
+
+    if (getData) {
+      this.taskData = JSON.parse(getData);
+    }
+  };
+
+  createTaskList = () => {
+    this.getFromLocal();
+    taskList.innerHTML = '';
+    this.taskData.forEach((task) => {
+      const li = document.createElement('li');
+      li.classList.add('list-item');
+
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.checked = task.completed;
+      checkbox.dataset.index = task.index;
+      checkbox.classList.add('checkbox');
+      li.appendChild(checkbox);
+
+      const taskText = document.createElement('span');
+      taskText.classList.add('task-text');
+      taskText.textContent = task.description;
+      taskText.dataset.index = task.index;
+      li.appendChild(taskText);
+
+      const delBtn = document.createElement('button');
+      delBtn.dataset.index = task.index;
+      delBtn.classList.add('del-btn');
+      li.appendChild(delBtn);
+
+      taskList.appendChild(li);
+    });
+  };
+
+  addTask = () => {
+    this.addData();
+    this.setToLocal();
+    this.createTaskList();
+  }
+
+  updateIndex = () => {
+    this.taskData.forEach((task, i) => {
+      task.index = i + 1;
+    });
+  }
+
+  deleteTask = (index) => {
+    this.taskData.splice(index, 1);
+    this.updateIndex();
+    this.setToLocal();
+    this.createTaskList();
+  }
+}
