8dc8e06a03924f4932a25179475b612f36008b1a
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-08-25 06:03:51 +0600

    Create update content functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import ToDo from './modules/todoList.js';
-import { taskList, inputTask } from './modules/variables.js';
+import { taskList, inputTask, clearButton } from './modules/variables.js';
+import { toggleCheckbox, clearCompletedTask, textDecoration } from './modules/updateList.js';
 
 const addButton = document.getElementById('add-btn');
 const todo = new ToDo();
@@ -19,4 +20,42 @@ taskList.addEventListener('click', (event) => {
   }
 });
 
-window.addEventListener('load', todo.createTaskList);
+taskList.addEventListener('change', (e) => {
+  if (e.target.classList.contains('checkbox')) {
+    const index = parseInt(e.target.dataset.index, 10) - 1;
+    toggleCheckbox(index, todo);
+    todo.createTaskList();
+    textDecoration(todo.taskData);
+  }
+});
+
+clearButton.addEventListener('click', () => {
+  todo.taskData = clearCompletedTask(todo.taskData);
+  todo.updateIndex();
+  todo.setToLocal();
+  todo.createTaskList();
+});
+
+taskList.addEventListener('dblclick', (e) => {
+  if (e.target.classList.contains('task-text')) {
+    if (!e.target.isContentEditable) {
+      e.target.contentEditable = true;
+      e.target.focus();
+    }
+  }
+});
+
+taskList.addEventListener('focusout', (e) => {
+  if (e.target.classList.contains('task-text')) {
+    const index = parseInt(e.target.dataset.index, 10) - 1;
+    todo.taskData[index].description = e.target.textContent.trim();
+    todo.setToLocal();
+    e.target.blur();
+    e.target.contentEditable = false;
+  }
+});
+
+window.addEventListener('load', () => {
+  todo.createTaskList();
+  textDecoration(todo.taskData);
+});
