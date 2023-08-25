e78d3f0d47cb074e637eb824a969f0a1c1bad8ae
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-08-25 14:36:08 +0600

    Testing the add function

---
+++ src/modules/__test__/todoList.test.js
@@ -0,0 +1,51 @@
+import ToDo from '../todoList.js';
+
+const todo = new ToDo();
+
+describe('todo list: test all functiona', () => {
+  document.body.innerHTML = ` <div class="form-wrapper">
+  <form action="#" class="main-form">
+    <div class="title-container">
+      <h1>Today's To Do</h1>
+    </div>
+    <div class="input-container">
+      <input type="text" name="input-task" id="input-task" placeholder="Add to your list" required>
+      <button type="submit" id="add-btn">&#x23CE</button>
+    </div>
+    <div class="task-wrapper">
+      <ul id="task-list">
+
+      </ul>
+    </div>
+    <div class="button-container">
+      <button id="clear-button">
+        Clear all completed
+      </button>
+    </div>
+  </form>
+  </div>`;
+
+  describe('Add new task', () => {
+    test('todo list should add a task per input', () => {
+      todo.addTask('Task 1');
+      todo.addTask('Task 2');
+      todo.addTask('Task 3');
+      expect(todo.taskData.length).toBe(3);
+    });
+
+    test('localStorage should contain all task data', () => {
+      todo.addTask('task 4');
+      todo.addTask('task 5');
+      const listdata = JSON.parse(localStorage.getItem('taskCollection'));
+      expect(listdata.length).toBe(5);
+    });
+
+    test('UI should display all task data', () => {
+      todo.addTask('Task 6');
+      const listdata = JSON.parse(localStorage.getItem('taskCollection'));
+      let listItem = document.querySelectorAll('.list-item');
+      listItem = listdata;
+      expect(listItem).toHaveLength(6);
+    });
+  });
+});
